88aed85bb364dd8ae8a3035b0a08f38c3ba18511
marvoqs <marvoqs@gmail.com>
2021-01-15 17:55:31 +0100

    add content styled component

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
+import { Content } from './components';
 import { unregister, reportWebVitals } from './core';
 import { GlobalStyles, theme } from './styles';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello World</div>
+    <Content>
+      <div>Hello World</div>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 );
