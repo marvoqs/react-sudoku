aaf6dd1ba5d098a7724bac494d55c0083d61f79e
marvoqs <marvoqs@gmail.com>
2021-01-15 14:58:18 +0100

    apply global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import { unregister, reportWebVitals } from './core';
 import { GlobalStyles } from './styles';
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <div>Hello World</div>
+  </>,
+  document.getElementById('root')
+);
 
 unregister();
 
