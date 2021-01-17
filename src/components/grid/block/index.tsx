2d9380b09c7dd653d07ac74b783217c701e7acba
marvoqs <marvoqs@gmail.com>
2021-01-17 10:15:54 +0100

    map numbers to grid

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { IReducer } from 'reducers';
+import { N } from 'typings';
 
 import { Container } from './styles';
 
@@ -6,14 +10,13 @@ interface IProps {
   colIndex: number;
   rowIndex: number;
 }
+interface IState {
+  value: N;
+}
 
 const Block: React.FC<IProps> = ({ colIndex, rowIndex }) => {
-  return (
-    <Container data-cy={`block-${rowIndex}-${colIndex}`}>
-      {rowIndex}
-      {colIndex}
-    </Container>
-  );
+  const state = useSelector<IReducer, IState>(({ grid }) => ({ value: grid ? grid[rowIndex][colIndex] : 0 }));
+  return <Container data-cy={`block-${rowIndex}-${colIndex}`}>{state.value === 0 ? '' : state.value}</Container>;
 };
 
 export default Block;
