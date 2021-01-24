4596b34963c5c82bdcff571c042aacc0dde9c61b
marvoqs <marvoqs@gmail.com>
2021-01-24 14:24:23 +0100

    style puzzle numbers differently than initial numbers

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -14,12 +14,14 @@ interface IProps {
 
 interface IState {
   isActive: boolean;
+  isPuzzle: boolean;
   value: N;
 }
 
 const Block: React.FC<IProps> = ({ colIndex, rowIndex }) => {
-  const state = useSelector<IReducer, IState>(({ workingGrid, selectedBlock }) => ({
+  const state = useSelector<IReducer, IState>(({ challengeGrid, selectedBlock, workingGrid }) => ({
     isActive: selectedBlock ? (selectedBlock[0] === rowIndex && selectedBlock[1] === colIndex ? true : false) : false,
+    isPuzzle: challengeGrid && challengeGrid[rowIndex][colIndex] !== 0 ? true : false,
     value: workingGrid ? workingGrid[rowIndex][colIndex] : 0,
   }));
   const dispatch = useDispatch<Dispatch<AnyAction>>();
@@ -28,7 +30,7 @@ const Block: React.FC<IProps> = ({ colIndex, rowIndex }) => {
     if (!state.isActive) dispatch(selectBlock([rowIndex, colIndex]));
   }
   return (
-    <Container active={state.isActive} data-cy={`block-${rowIndex}-${colIndex}`} onClick={handleClick}>
+    <Container active={state.isActive} data-cy={`block-${rowIndex}-${colIndex}`} onClick={handleClick} puzzle={state.isPuzzle}>
       {state.value === 0 ? '' : state.value}
     </Container>
   );
